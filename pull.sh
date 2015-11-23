950c3ee2c8b10e9e8167b8cd9ecdc14ecf6fa934
Andrew Rothstein <arothste@blackrock.com>
2015-11-23 08:48:39 -0500

    convenience script for pulling down all the containers

---
+++ pull.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker pull andrewrothstein/docker-ansible:ubuntu_trusty
+docker pull andrewrothstein/docker-ansible:fedora_21
+docker pull andrewrothstein/docker-ansible:centos_7.1.1503
+
