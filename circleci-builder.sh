89d823a2183c34a2ee33fb2d6d61f2b9954fb948
Andrew Rothstein <andrew.rothstein@gmail.com>
2016-01-23 23:58:01 -0500

    fix os name search

--- circleci-builder.sh
+++ circleci-builder.sh
@@ -6,7 +6,7 @@ DOCKERCONTAINER=docker-ansible
 
 i=0
 files=()
-for os in $(find . -name Dockerfile -printf '%h\n' | sort); do
+for os in $(find . -name Dockerfile -printf '%h\n' | sed 's/^\.\///' | sort); do
     if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
     then
 	CONTAINERNAME=$DOCKERUSER/${DOCKERCONTAINER}_$os
