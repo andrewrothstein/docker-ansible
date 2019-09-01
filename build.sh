f53faf6cdf49e667723560dce177cae666ad9915
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-01 16:24:21 -0500

    bit more logging

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ printf "os: %s\n" $os
 printf "dotver: %s\n" $dotver
 printf "dashver: %s\n" $dashver
 
-source ./ansible-install-lib
+ls -l ./ansible-install-lib
+. ./ansible-install-lib
 
 write_dockerfile $os $dotver $dashver
 docker build -t $TARGET_REGISTRY/$TARGET_GROUPNAME/$TARGET_NAME:${os}_${dotver} -f Dockerfile.${os}_${dotver} .
