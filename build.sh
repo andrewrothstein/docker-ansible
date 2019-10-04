537825887865f00fc48fa8d4d069035885dbef1f
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-03 23:19:45 -0500

    add a ubi8 CI build

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 
 TARGET_REGISTRY=quay.io
 TARGET_GROUPNAME=andrewrothstein
@@ -9,13 +8,9 @@ os=$1
 dotver=$2
 dashver=$3
 
-printf "os: %s\n" $os
-printf "dotver: %s\n" $dotver
-printf "dashver: %s\n" $dashver
-
 . ./ansible-install-lib
 
-write_dockerfile $os $dotver $dashver
+write_dockerfile_$os $dotver $dashver
 docker build \
        --build-arg HTTP_PROXY --build-arg HTTPS_PROXY --build-arg NO_PROXY \
        --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy \
