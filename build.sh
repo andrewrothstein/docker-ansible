84775e16bd4b2f8360adc34a8cb2d29918a90801
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-11 09:42:00 -0500

    first try

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ dashver=$3
 . ./ansible-install-lib
 
 write_dockerfile_$os $dotver $dashver
-docker build \
+podman build \
        --build-arg HTTP_PROXY --build-arg HTTPS_PROXY --build-arg NO_PROXY \
        --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy \
        -t $TARGET_REGISTRY/$TARGET_GROUPNAME/$TARGET_NAME:${os}_${dotver} \
