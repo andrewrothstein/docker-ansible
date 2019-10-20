c8388914447bce2c3203b4561c73f551966edbb2
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-19 21:16:52 -0500

    pip3 upgrade pip is sadness. parameterize podman/docker and other  target image parameters

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-TARGET_REGISTRY=quay.io
-TARGET_GROUPNAME=andrewrothstein
-TARGET_NAME=docker-ansible
+target_registry=${TARGET_REGISTRY:-quay.io}
+target_groupname=${TARGET_GROUPNAME:-andrewrothstein}
+target_name=${TARGET_NAME:-docker-ansible}
+
+container_builder=${CONTAINER_BUILDER:-podman}
 
 os=$1
 dotver=$2
@@ -11,9 +13,9 @@ dashver=$3
 . ./ansible-install-lib
 
 write_dockerfile_$os $dotver $dashver
-podman build \
+$container_builder build \
        --build-arg HTTP_PROXY --build-arg HTTPS_PROXY --build-arg NO_PROXY \
        --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy \
-       -t $TARGET_REGISTRY/$TARGET_GROUPNAME/$TARGET_NAME:${os}_${dotver} \
+       -t ${target_registry}/${target_groupname}/${target_name}:${os}_${dotver} \
        -f Dockerfile.${os}_${dotver} \
        .
