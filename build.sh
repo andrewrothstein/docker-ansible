b646fff394ef10b251847c71db343f446d1745ae
Andrew Rothstein <andrewrothstein@users.noreply.github.com>
2021-11-07 19:34:49 -0500

    Publish images to ghcr too! (#9)
    * tag images to ghcr
    * add ghcr login + push

--- build.sh
+++ build.sh
@@ -3,12 +3,19 @@ set -e
 #set -x
 
 target_registry=${TARGET_REGISTRY:-quay.io}
-target_groupname=${TARGET_GROUPNAME:-andrewrothstein}
-target_name=${TARGET_NAME:-docker-ansible}
+target_registry2=${TARGET_REGISTRY2:-ghcr.io}
+target_slug=${TARGET_SLUG:-"andrewrothstein/docker-ansible"}
+
+target_image_name="${target_registry}/${target_slug}"
+target_image_name2="${target_registry2}/${target_slug}"
 
 os=$1
 dotver=$2
 dashver=$3
+tag_underbar="${os}_${dotver}"
+
+fq_image_name="${target_image_name}:${tag_underbar}"
+fq_image_name2="${target_image_name2}:${tag_underbar}"
 
 . ./ansible-install-lib
 
@@ -16,8 +23,13 @@ write_dockerfile_$os $dotver $dashver
 sudo \
     buildah \
     bud \
-    -f Dockerfile.${os}_${dotver} \
+    -f Dockerfile.${tag_underbar} \
     --build-arg HTTP_PROXY --build-arg HTTPS_PROXY --build-arg NO_PROXY \
     --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy \
-    -t ${target_registry}/${target_groupname}/${target_name}:${os}_${dotver} \
+    -t ${fq_image_name} \
     .
+sudo \
+    buildah \
+      tag \
+      $fq_image_name \
+      $fq_image_name2
