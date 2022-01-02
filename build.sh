6993f89b812344a78eac1c3a79d61d1359e3aacf
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-01-02 17:32:48 -0500

    simplify dashver vs dotver chicanery

--- build.sh
+++ build.sh
@@ -10,15 +10,14 @@ target_image_name="${target_registry}/${target_slug}"
 target_image_name2="${target_registry2}/${target_slug}"
 
 os=$1
-dotver=$2
-dashver=$3
-tag_underbar="${os}_${dotver}"
+os_ver=$2
+tag_underbar="${os}_${os_ver}"
 
 fq_image_name="${target_image_name}:${tag_underbar}"
 fq_image_name2="${target_image_name2}:${tag_underbar}"
 
 . ./ansible-install-lib
 
-write_dockerfile_$os $dotver $dashver
+write_dockerfile_$os $os_ver
 buildah_build_and_tag Dockerfile.${tag_underbar} ${fq_image_name} ${fq_image_name2}
 #docker_build_and_tag Dockerfile.${tag_underbar} ${fq_image_name} ${fq_image_name2}
--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 set -e
 #set -x
 
-# example: ./install.sh alpine 3_10
+# example: ./install.sh alpine 3.15
 
 os=$1
-dashver=$2
+os_ver=$2
 
 . ./ansible-install-lib
 
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-install_${os}_${dashver}
+install_${os} ${os_ver}
